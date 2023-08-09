606e286d73cfa7b29a91ca7ce79e9882203c3ea9
nrkycn <narikakun@gmail.com>
2023-08-09 22:06:11 +0900

    通知システムの開発

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'firebase_options.dart';
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
-  final flutterLocalNotificationsPluginO = FlutterLocalNotificationsPlugin();
-
-  await flutterLocalNotificationsPluginO.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()!.requestPermission();
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-    RemoteNotification? notification = message.notification;
-    print(notification?.title);
-    print(notification?.body);
-    const settings = InitializationSettings(
-      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
-    );
-    await flutterLocalNotificationsPluginO.initialize(settings);
-    const details = NotificationDetails(
-      android: AndroidNotificationDetails("EarthquakeInfo", "地震情報"),
-    );
-    await flutterLocalNotificationsPluginO.show(0, notification?.title, notification?.body, details);
-  });
-  print("Handling a background message: ${message.messageId}");
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseMessaging.instance.requestPermission();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  //NotificationFunction();
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true
+  );
+  await FirebaseMessaging.instance.subscribeToTopic("quakeInfo");
+  FirebaseMessaging.onMessage.listen(firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
--- lib/src/utils/Notification.dart
+++ lib/src/utils/Notification.dart
@@ -2,13 +2,29 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void NotificationFunction () async {
-  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
 
-  var messagingToken = await _firebaseMessaging.getToken();
-  print(messagingToken);
-
-  await _firebaseMessaging.subscribeToTopic("earthquakeNotification");
-
-
+  final flutterLocalNotificationsPluginO = FlutterLocalNotificationsPlugin();
+  RemoteNotification? notification = message.notification;
+  print(message.hashCode);
+  const settings = InitializationSettings(
+    android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+  );
+  await flutterLocalNotificationsPluginO.initialize(settings);
+  const details = NotificationDetails(
+    android: AndroidNotificationDetails(
+      "default_notification_channel_quakeinfo",
+      "地震情報",
+      channelDescription: "地震情報を通知します",
+      importance: Importance.max,
+      priority: Priority.high,
+      timeoutAfter: 5000,
+      styleInformation: DefaultStyleInformation(true, true),
+      playSound: true,
+    ),
+  );
+  await flutterLocalNotificationsPluginO.show(message.hashCode, notification?.title, notification?.body, details);
+  print("Handling a background message: ${message.messageId}");
 }
